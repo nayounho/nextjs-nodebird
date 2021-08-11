a802466cba4ae60c4f601700147ade3815f2e61d
younhona <younho.na@atto-research.com>
2021-08-11 19:30:33 +0900

    [추가] nextjs-nodebird project 생성
    1. test

---
+++ front/pages/index.js
@@ -0,0 +1,12 @@
+import AppLayout from "./components/AppLayout";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
